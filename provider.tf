a8d434b94558f88d60dd60a791335f1ccba6b1d5
OlugbemigaAdefuye <86925721+OlugbemigaAdefuye@users.noreply.github.com>
2024-07-07 16:57:37 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-        source = "hashicorp/aws"
-        version = "4.67.0"
-    }
-  }
-}
-
 provider "aws" {
   region = var.region
-  #profile = "default"
 }
